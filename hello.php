c2d0674489a1d03bbe480b636b794be3251e60f6
Jordan Sinou <sinoujordan@gmail.com>
2022-03-21 11:59:10 +0100

    test integration and variable

---
+++ hello.php
@@ -0,0 +1,4 @@
+<?php
+$name = $_GET['name'];
+
+echo "<h2>Hello $name</h2>";
--- tests/IntegrationsTest.php
+++ tests/IntegrationsTest.php
@@ -2,10 +2,28 @@
 require 'vendor/autoload.php';
 
 class IntegrationsTest extends IntegrationTestCase {
-    public function test_index() {
-        $response = $this->make_request("GET", "/");
+    // public function test_index() {
+    //     $response = $this->make_request("GET", "/");
+    //     $this->assertEquals("200", $response->getStatusCode());
+    //     $this->assertEquals("Hello World!", $response->getBody()->getContents());
+    //     $this->assertStringContainsString("text/html", $response->getHeader('Content-Type')[0]);
+    // }
+
+    /**
+     * @dataProvider provider
+     */
+    public function test_hello($name) {
+        $response = $this->make_request("GET", "/hello.php?name=$name");
         $this->assertEquals("200", $response->getStatusCode());
-        $this->assertEquals("Hello World!", $response->getBody()->getContents());
+        $this->assertEquals("<h2>Hello $name</h2>", $response->getBody()->getContents());
         $this->assertStringContainsString("text/html", $response->getHeader('Content-Type')[0]);
+    }
+
+    public function provider() {
+        return [
+            ['Test'],
+            ['Doe'],
+            ['Dodo']
+        ];
     }
 }
